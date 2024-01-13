c6edeb8d5fa4b3d44bad4a358f76998607af1bd0
Hardjuno Indracahya <71756948+HardjunoIndracahya@users.noreply.github.com>
2024-01-13 23:11:49 +0700

    update

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 					<div class="mb-6 md:mb-0">
 						<a href="#" class="flex items-center">
 							<img
-								src="/public/logo-aba.png"
+								src="/logo-aba.png"
 								alt=""
 								className="h-[4.5rem] w-[4.5rem] right-2 relative rounded-full brightness-200"
 							/>
@@ -44,13 +44,8 @@ const Footer = () => {
 							<h2 class="mb-4 text-sm font-semibold uppercase text-white">Follow us</h2>
 							<ul class="text-gray-400 dark:text-gray-400 font-medium">
 								<li class="mb-2">
-									<a href="https://www.instagram.com/acetkjthree/?hl=id" class="hover:underline ">
+									<a href="https://www.instagram.com/abaclass.ofc/" class="hover:underline ">
 										Instagram
-									</a>
-								</li>
-								<li>
-									<a href="https://www.youtube.com/@11TKJ3" class="hover:underline">
-										Youtube
 									</a>
 								</li>
 							</ul>
@@ -69,7 +64,7 @@ const Footer = () => {
 				<div class="sm:flex sm:items-center sm:justify-between">
 					<div class="flex mt-4 justify-center sm:mt-0">
 						<p className="text-[0.7rem] text-white opacity-70">
-							© {new Date().getFullYear()} Kelas XI TKJ 3 | Di Kelola Oleh Siswa TKJ 3
+							© {new Date().getFullYear()} Kelas IX ABA | Di Kelola Oleh Siswa IX ABA
 						</p>
 					</div>
 				</div>
